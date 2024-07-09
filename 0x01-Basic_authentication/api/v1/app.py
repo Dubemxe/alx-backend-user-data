06bc68cf4c11a3ef2ff1b1c120da72c2ca57f6d4
Dubemxe <dubemxe@gmail.com>
2024-07-09 13:36:22 +0000

    corrections

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,8 +20,9 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
-def unauthorized_acc(error) -> str:
+def unauthorized_acc(error):
     """ Unauthorized error handler """
     return jsonify({"error": "Unauthorized"}), 401
 
