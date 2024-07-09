2b69fcd9e963a0e3dd4447097b1ded7fc3f712d4
Dubemxe <dubemxe@gmail.com>
2024-07-09 13:26:24 +0000

    api/v1 update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_err():
+    """ Aborts and returns an unauthorized
+    access error"""
+    abort(401)
